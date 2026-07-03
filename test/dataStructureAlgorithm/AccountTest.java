fc38b52d69454d2a0e9796e647df6b7c3d35cc89
Midemee <ashiwajuaramide@gmail.com>
2026-07-03 16:00:42 +0100

    update

--- test/dataStructureAlgorithm/AccountTest.java
+++ test/dataStructureAlgorithm/AccountTest.java
@@ -1,7 +1,8 @@
-package account;
+package dataStructureAlgorithm;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,30 +19,30 @@ public class AccountTest {
     @Test
     public void testThatIHaveAnAccountBalanceIsZeroIfIDeposit200BalanceBecomes200() {
         myAccount.deposit(200);
-        Assertions.assertEquals(200, myAccount.getBalance(4321));
+       assertEquals(200, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatIHaveAnAccountBalanceIsZeroIfIDeposANegativeAmountBalanceRemainsZero() {
         myAccount.deposit(-500);
-        Assertions.assertEquals(0, myAccount.getBalance(4321));
+       assertEquals(0, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatIHaveAnAccountBalanceIsZeroIfIWithdraw500BalanceRemainsZero() {
         myAccount.withdraw(300, 4321);
-        Assertions.assertEquals(0, myAccount.getBalance(4321));
+        assertEquals(0, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatWhenICheckMyBalanceWithCorrectPinIGetMyBalance(){
         myAccount.deposit(500);
-        Assertions.assertEquals(500, myAccount.getBalance(4321));
+        assertEquals(500, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatWhenICheckMyBalanceWithWrongPinItThrowsError(){
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             myAccount.getBalance(1234);
         });
     }
@@ -50,26 +51,26 @@ public class AccountTest {
     public void testThatIHaveAnAccountBalanceIs500IfIWithdraw300BalanceIs200 () {
         myAccount.deposit(500);
         myAccount.withdraw(300, 4321);
-        Assertions.assertEquals(200, myAccount.getBalance(4321));
+        assertEquals(200, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatWhenIWithdrawANegativeAmountBalanceDoesNotChange() {
         myAccount.deposit(1000);
         myAccount.withdraw(-500, 4321);
-        Assertions.assertEquals(1000, myAccount.getBalance(4321));
+        assertEquals(1000, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatWhenIWithdrawFromAnEmptyAccountBalanceDoesNotChange() {
         myAccount.withdraw(600, 4321);
-        Assertions.assertEquals(0, myAccount.getBalance(4321));
+       assertEquals(0, myAccount.getBalance(4321));
     }
 
     @Test
     public void testThatWhenIWithdrawWithWrongPinItThrowsError(){
         myAccount.deposit(600);
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             myAccount.withdraw(300, 1234);
         });
     }
